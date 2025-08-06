b19d3f563ce6aa9ae7b253ffe1a2b23f0f60f72a
DJ <gk22719@gmail.com>
2025-08-06 18:17:05 +0530

    added test-stream

---
+++ app/api/stream/route.ts
@@ -0,0 +1,20 @@
+import {streamText} from "ai";
+import { google } from "@ai-sdk/google";
+export async function POST(req: Request) {
+	try {
+		const {prompt} = await req.json();
+	if(!prompt) {
+		return new Response("Prompt is required", { status: 400 });
+	}
+	const result = streamText({
+		model: google("models/gemini-2.0-flash-exp"),
+		prompt
+	})
+	return result.toUIMessageStreamResponse();
+	} catch (error) {
+		console.log("Error in completion route:", error);
+		return Response.json({
+			error: "Failed to generate text"
+		}, {status: 500})
+	}
+}
---
+++ app/ui/stream/page.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { useCompletion } from "@ai-sdk/react";
+
+export default function StreamPage() {
+	const {input, handleInputChange, handleSubmit, completion, isLoading, error, setInput, stop} = useCompletion({
+		api: "/api/stream",
+	});
+
+	return (
+		<div className="flex flex-col w-full h-screen mx-auto p-4">
+			<div className="flex-1 w-full max-w-4xl mx-auto p-4 mb-20 overflow-y-auto border border-neutral-500 rounded-xl">
+				{error && <div className="text-red-500 mb-4 text-sm">Error: {error.message}</div>}
+				{isLoading && !completion && <div>Loading...</div>}
+				{completion && <div>{completion}</div>}
+			</div>
+			<form className="flex gap-2 fixed bottom-10 w-full p-2" onSubmit={(e) => {
+				e.preventDefault();
+				setInput("");
+				handleSubmit(e);
+			}}>
+				<div className="mx-auto">
+					<input placeholder="Ask me anything..."
+					value={input}
+					onChange={handleInputChange}
+					type="text"
+						className="flex-1 dark:bg-zinc-800 p-2 pl-4 border-zinc-300 dark:border-zinc-100 rounded-l-lg"
+					/>
+					{isLoading ? <button onClick={stop} className="cursor-pointer py-2 px-4 bg-red-500 text-white rounded-r-lg">Stop</button>
+					 : 
+					 <button
+					  disabled={isLoading}
+						type="submit"
+						className="py-2 px-4 bg-blue-500 text-white hover:bg-blue-600 rounded-r-lg"
+						>
+						Send
+					</button>
+					 }
+				</div>
+			</form>
+		</div>
+	)
+}
